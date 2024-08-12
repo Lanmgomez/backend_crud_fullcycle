66b8df1656444e17bfcdf3013ee2cf1a84c44002
Islan Gomes da Silva <islan.gomes@supplylabs.com.br>
2024-08-12 19:04:40 -0300

    habilitando cors

--- router/router.go
+++ router/router.go
@@ -3,11 +3,25 @@ package router
 import (
 	"github.com/Lanmgomez/backend_crud_fullcycle/internal/domain/user"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routers() *gin.Engine {
 	router := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+
+	router.Use(cors.New(config))
 
 	router.GET("/users", user.GetUsers)
 

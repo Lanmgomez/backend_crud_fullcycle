850afa310c1d69a5399d8357e60efbc5cec8b8b3
Islan Gomes da Silva <islan.gomes@supplylabs.com.br>
2024-08-13 15:09:45 -0300

    habilitando CORS para todos os headers origins

--- router/router.go
+++ router/router.go
@@ -1,9 +1,9 @@
 package router
 
 import (
-	"github.com/Lanmgomez/backend_crud_fullcycle/internal/domain/user"
-
 	"time"
+
+	"github.com/Lanmgomez/backend_crud_fullcycle/internal/domain/user"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -13,9 +13,9 @@ func Routers() *gin.Engine {
 	router := gin.Default()
 
 	config := cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,

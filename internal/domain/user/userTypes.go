3d7f314ba2fa439b1f4628006a0d793c52eeeff8
Islan Gomes da Silva <islan.gomes@supplylabs.com.br>
2024-10-14 19:39:16 -0300

    finalizando task

--- internal/domain/user/userTypes.go
+++ internal/domain/user/userTypes.go
@@ -35,11 +35,36 @@ type LOGINLOGS struct {
 	IpAddress string `json:"ipAddress"`
 }
 
+type USER_IDENTIFICATION_CONTACT struct {
+	Id            int64  `json:"id"`
+	FullName      string `json:"fullName"`
+	CpfOrCnpj     string `json:"cpfOrCnpj"`
+	Email         string `json:"email"`
+	Phone         string `json:"phone"`
+	Address       string `json:"address"`
+	AddressNumber string `json:"addressNumber"`
+	Complement    string `json:"complement"`
+	Neighborhood  string `json:"neighborhood"`
+	City          string `json:"city"`
+	Uf            string `json:"uf"`
+	ZipCode       string `json:"zipCode"`
+}
+
+type PAYMENT_METHOD struct {
+	Id                     int64  `json:"id"`
+	PaymentUserId          int64  `json:"paymentUserId"`
+	PaymentFormInstallment string `json:"paymentFormInstallment"`
+	Token                  string `json:"token"`
+	DateTime               string `json:"dateTime"`
+}
+
 type PAYMENTS struct {
-	Id            int    `json:"id"`
-	UserPaymentID int    `json:"userPaymentId"`
-	UserPay       string `json:"userPay"`
-	Status        string `json:"status"`
-	Token         string `json:"token"`
-	PaymentDate   string `json:"paymentDate"`
+	UserIdentification USER_IDENTIFICATION_CONTACT
+	PaymentForm        PAYMENT_METHOD
+}
+
+type UF_STATES struct {
+	Id    int    `json:"id"`
+	State string `json:"state"`
+	Uf    string `json:"uf"`
 }
